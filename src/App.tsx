2b41d7eb088de0463db3cfdf8273403b067a2c71
Nikita Alpatov <nikitaa95@mail.ru>
2023-05-25 13:51:52 +0300

    Check

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./components/Technologies/Technologies";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header/>
+        <Technologies/>
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <a href='#'> Home</a>
+            <a href='#'> News Feed</a>
+            <a href='#'> Messages</a>
+            <a href='#'> More</a>
+        </div>
+    )
+}
+
+export default Header;
